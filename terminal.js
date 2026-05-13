35cd5b53ef0c28ab832f07f963fe9950ce19797f
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-05-13 12:39:08 +0900

    feat: コマンドを受け付ける機能を追加

--- terminal.js
+++ terminal.js
@@ -5,24 +5,63 @@ const terminalElement = document.getElementById('terminal');
 
 // ターミナルを起動する
 const term = new Terminal({
-  corsorBlink: true,
-  rows: 10,
-  cols: 80,
-  thema: {
-    background: '#1e1e1e'
-  }
+    corsorBlink: true,
+    rows: 10,
+    cols: 80,
+    thema: {
+        background: '#1e1e1e'
+    }
 });
 
 term.open(terminalElement);
 term.writeln("terminal on BROWSER!")
 term.write("$ ");
 
+// コマンド一覧
+const commands = {
+    help: () => {
+        term.write('\r\n[Available Commands]\r\n - help  : Show this message\r\n - clear : Clear terminal');
+    },
+    clear: () => {
+        term.clear();
+    }
+}
+
+// 入力された文字列をためておく変数
+let currentLine = "";
+
+// 入力を監視するリスナー
 term.onData(data => {
-  if(data === "\r") {
-    term.write("\r\n$");
-  } else if(data === "\x7F") {
-    term.write("\b \b");
-  } else {
-    term.write(data);
-  }
+
+    // エンター（改行コード）が押されたとき
+    if(data === "\r") {
+
+        const command = currentLine.trim();
+
+        if(command in commands) {
+            commands[command]();
+        }
+        else if(command !== "") {
+            term.write(`\r\nCommand not found: ${command}`);
+        }
+
+        // エンターしたらcurrentLineを空にする
+        currentLine = "";
+
+        term.write("\r\n$ ");
+    }
+    
+    // バックスペース（デリートコード）が押されたとき
+    else if(data === "\x7F") {
+        if(currentLine.length > 0) {
+            currentLine = currentLine.slice(0, -1);
+            term.write("\b \b");
+        }
+    }
+
+    // それ以外の通常の入力のとき
+    else {
+        currentLine += data;
+        term.write(data);
+    }
 });
